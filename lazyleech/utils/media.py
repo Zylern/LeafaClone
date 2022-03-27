eb917ba51d8d5f0855e06b812d72910b8725c6d5
Zylern <47510180+Zylern@users.noreply.github.com>
2022-03-27 10:23:16 +0530

    Add files via upload

--- lazyleech/utils/media.py
+++ lazyleech/utils/media.py
@@ -7,12 +7,12 @@ from html_telegraph_poster import TelegraphPoster
 def post_to_telegraph(a_title: str, content: str) -> str:
     """ Create a Telegram Post using HTML Content """
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "Zylern"
+    auth_name = "𝗔𝗹𝗶𝘀𝗮 𝗜𝗹𝗶𝗰𝗵𝗶𝗻𝗮"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=a_title,
         author=auth_name,
-        author_url="https://t.me/Zylern",
+        author_url="https://t.me/AlisaAmiellaBot",
         text=content,
     )
     return post_page["url"]
